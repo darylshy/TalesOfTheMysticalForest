9d50d7fc6037dbd939653a8f0ec5b8c5fd88b794
darylshy <darylshy@yahoo.com>
2017-01-18 23:32:11 -0500

    modal text and width adjustments; subtext button width adjusted

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,12 @@
-var $story = $(".story");
-var $subtextBtn = $(".subtext-btn");
+var $container = $(".container-fluid");
+var $story = $container.find(".story");
+var $subtext = $container.find(".subtext");
+var $subtextBtn = $container.find(".subtext-btn");
 var subtextBtnText = $subtextBtn.prop('value');
+var $headline = $container.find(".headline");
+var $storyTitle = $container.find(".story .title");
+var $modalHeader = $container.find(".modal-content .modal-header .modal-title.h1");
+var $modalBody = $container.find(".modal-content .modal-body");
 //----------------------------------------------------
 function btnOver() {
     $subtextBtn.prop('value', subtextBtnText + " Alive");
@@ -12,9 +18,11 @@ function btnOut() {
 }
 $subtextBtn.hover(btnOver,btnOut);
 
-$(".headline").fitText(2, { minFontSize: '18px', maxFontSize: '84px' });
-$(".subtext").fitText(4, { minFontSize: '12px', maxFontSize: '36px' });
-$(".story .title").fitText(2, {minFontSize:'12px', maxFontSize:'36px'});
+$headline.fitText(2, { minFontSize: '18px', maxFontSize: '84px' });
+$subtext.fitText(4, { minFontSize: '12px', maxFontSize: '36px' });
+$storyTitle.fitText(2, {minFontSize:'12px', maxFontSize:'36px'});
+$modalHeader.fitText(2, {minFontSize:'12px', maxFontSize:'36px'});
+$modalBody.fitText(6, {minFontSize:'12px', maxFontSize:'36px'});
 
 
 
