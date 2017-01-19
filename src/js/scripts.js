68e212b9680ea5e9abf5bb63e6ed81a05de4b5be
darylshy <darylshy@yahoo.com>
2017-01-19 06:57:16 -0500

    added a summary to the footer for each story to mobile and desktop. created 2 unique UX designs, and  cool animations and transitions - ready to merge

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,17 @@
-var $body = $("body");
-var $container = $body.find(".container-fluid");
-var $story = $container.find(".story");
-var $subtext = $container.find(".subtext");
-var $subtextBtn = $container.find(".subtext-btn");
-var subtextBtnText = $subtextBtn.prop('value');
-var $headline = $container.find(".headline");
-var $storyTitle = $container.find(".story .title");
-var $modalHeader = $container.find(".modal-content .modal-header .modal-title.h1");
-var $modalBody = $container.find(".modal-content .modal-body");
-var $footer = $body.find("footer.text-muted");
+let $body = $("body");
+let $container = $body.find(".container-fluid");
+let $story = $container.find(".story");
+let $storyImg = $story.find('.story-img');
+let $subtext = $container.find(".subtext");
+let $subtextBtn = $container.find(".subtext-btn");
+let subtextBtnText = $subtextBtn.prop('value');
+let $headline = $container.find(".headline");
+let $storyTitle = $container.find(".story .title");
+let $modalHeader = $container.find(".modal-content .modal-header .modal-title.h1");
+let $modalBody = $container.find(".modal-content .modal-body");
+let $footer = $body.find("footer.text-muted");
+let $summary = $footer.find('.summary');
+let $arrow;
 //Animations----------------------------------------------------
 function btnOver() {
     $subtextBtn.prop('value', subtextBtnText + " Alive");
@@ -20,11 +23,54 @@ function btnOut() {
 }
 $subtextBtn.hover(btnOver,btnOut);
 
+
 $headline.fitText(2, { minFontSize: '18px', maxFontSize: '84px' });
 $subtext.fitText(4, { minFontSize: '12px', maxFontSize: '36px' });
 $storyTitle.fitText(2, {minFontSize:'12px', maxFontSize:'36px'});
 $modalHeader.fitText(2, {minFontSize:'12px', maxFontSize:'36px'});
 $modalBody.fitText(6, {minFontSize:'12px', maxFontSize:'36px'});
+$footer.fitText(6, {minFontSize:'12px', maxFontSize:'36px'});
+
+function handleStoryEventStart(e) {
+    let getSummary = $.get("../mock/summaries.json");
+    let storyWithFocus = e.target.className.match(/story\d/)[0];
+    let storyNum = parseInt(storyWithFocus.match(/\d/)[0]);
+    getSummary.then(function (result) {
+        $summary.html(result[storyNum-1].summary);
+    });
+    $arrow = $footer.find(`.arrow${storyNum}`);
+    $arrow.show();
+    $summary.slideDown('fast', function () {
+
+    });
+}
+
+function handleStoryEventEnd(e) {
+    e.preventDefault();
+    $arrow.hide();
+    $summary.slideUp('fast', function () {});
+
+}
+
+function stopStoryImgBubble(e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+}
+$storyImg.on('touchstart',stopStoryImgBubble);
+$storyImg.on('touchend',stopStoryImgBubble);
+$story.hover(handleStoryEventStart,handleStoryEventEnd);
+$story.on("touchstart", function(e){
+    handleStoryEventStart(e);
+    let storyNum = e.target.className.match(/\d\d?/g)[2];
+    $(this).find(".story-img").addClass(`story${storyNum}-img-hover`);
+});
+$story.on("touchend", function(e){
+    handleStoryEventEnd(e);
+    let storyImg = $(this).find('.story-img');
+    let storyImgClass = $(storyImg).prop('class');
+    storyImg.removeClass(`${storyImgClass.match(/story\d-img-hover/)[0]}`);
+});
+
 
 
 
